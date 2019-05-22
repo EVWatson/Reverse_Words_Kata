fa22765042c8cff18fbc2e3717179e148c1cd91e
EVWatson <emily.watson@myob.com>
2019-05-22 10:41:49 +1000

    test to reverse sentence string passes

--- src/String_Reverser.java
+++ src/String_Reverser.java
@@ -1,4 +1,23 @@
 public class String_Reverser {
 
+    /*
+    Ways to reverse strings:
+
+    1. separate into array of words (regex on white space), then loop through backwards. discovered .trim() method to get rid of space at end of new string
+    2. array.reverse method?
+    3. StringBuilder .reverse method
+    4. Pattern class
+     */
+
+
+    public String reverseString(String sentence){
+        String[] sentenceSplitIntoWords = sentence.split(" ");
+        String sentenceInReverse = "";
+        for(int word = sentenceSplitIntoWords.length -1; word >= 0; word --){
+           sentenceInReverse = sentenceInReverse.concat(sentenceSplitIntoWords[word] + " ");
+        }
+        return sentenceInReverse.trim();
+    }
+
 
 }
--- tests/String_ReverserTest.java
+++ tests/String_ReverserTest.java
@@ -1,8 +1,17 @@
+import jdk.jfr.Name;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class String_ReverserTest {
+
+    private String_Reverser string_reverser;
+
+    @Before
+    public void setUp(){
+        this.string_reverser = new String_Reverser();
+    }
 
     @Test
     public void whenGivenSingleStringSentenceReturnsWordsInReverse(){
@@ -11,7 +20,9 @@ public class String_ReverserTest {
 
          String expectedResult = "makers music the are we";
 
-         String actualResult = reverseString(singleSentence);
+         String actualResult = string_reverser.reverseString(singleSentence);
+
+        System.out.println(actualResult);
 
          assertEquals(expectedResult, actualResult);
 
